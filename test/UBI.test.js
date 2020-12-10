a08353ce57362c227e828891b64d9b86057503f8
Santiago Siri <santisiri@gmail.com>
2020-12-10 17:01:59 +0100

    token v0.1 ready

--- test/UBI.test.js
+++ test/UBI.test.js
@@ -1,9 +1,14 @@
+const BigNumber = web3.BigNumber;
 const UBI = artifacts.require('UBI');
+
+require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
 contract('UBI', accounts => {
   const _name = "Democracy Earth";
   const _symbol = "UBI";
-  const _decimals = 18;
+  const _decimals = web3.utils.toBN('18');
   const _supply = 10000;
 
   beforeEach(async () => {
@@ -11,16 +16,19 @@ contract('UBI', accounts => {
   });
 
   describe('token attribtues', () => {
-    it('has the correct name', () => {
-
+    it('has the correct name', async () => {
+      const name = await this.token.name();
+      name.should.equal(_name);
     });
 
-    it('has the correct symbol', () => {
-
+    it('has the correct symbol', async () => {
+      const symbol = await this.token.symbol();
+      symbol.should.equal(_symbol);
     });
 
-    it('has the correct decimals', () => {
-
+    it('has the correct decimals', async () => {
+      const decimals = await this.token.decimals();
+      decimals.should.be.bignumber.eql(_decimals);
     });
   });
 })
