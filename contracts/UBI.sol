d050a06453aea5b119dec1903840aeb8d0c0142e
Santiago Siri <santisiri@gmail.com>
2020-12-10 14:37:25 +0100

    first compile

--- contracts/UBI.sol
+++ contracts/UBI.sol
@@ -1,2 +1,10 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract UBI is ERC20 {
+  constructor(uint256 initialSupply) public ERC20("Democracy Earth", "UBI") {
+      _mint(msg.sender, initialSupply);
+  }
+}
