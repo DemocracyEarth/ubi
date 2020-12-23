8aa07b92695242aaca7676ac53989193efabf3da
santisiri <santisiri@gmail.com>
2020-12-23 19:16:15 +0100

    proof of humanity merge with token

--- contracts/UBI.sol
+++ contracts/UBI.sol
@@ -3,25 +3,108 @@ pragma solidity ^0.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
+/**
+ * @title ProofOfHumanity Interface
+ * @dev See https://github.com/Proof-Of-Humanity/Proof-Of-Humanity.
+ */
+interface IProofOfHumanity {
+    enum Status {None, Vouching, PendingRegistration, PendingRemoval}
+
+    function getSubmissionInfo(address _submissionID)
+        external
+        view
+        returns (
+            Status status,
+            uint64 submissionTime,
+            uint64 renewalTimestamp,
+            uint64 index,
+            bool registered,
+            bool hasVouched,
+            uint256 numberOfRequests
+        );
+}
+
+
 contract UBI is ERC20Burnable  {
-  uint256 public mintingPerSecond; // how many tokens per second will be minted for every valid human proof
+  
+  uint256 public accruedPerSecond; /// @dev How many tokens per second will be minted for every valid human proof per second.
+  IProofOfHumanity public immutable proofOfHumanity; /// @dev The Proof Of Humanity registry to reference.
 
-  mapping(address => uint256) public lastMintedTime; // persists time of last minted tokens
+  mapping(address => uint256) public lastMintedSecond; /// @dev Persists time of last minted tokens for any given address.
 
+  /* Modifiers */
+
+  /** @dev is Registered as Proof of Human.
+  *  @param _submissionID for the address of the human.
+  *  @param _registered if it's registered as valid human.
+  */
+  modifier isRegistered(address _submissionID, bool _registered) {
+      (, , , , bool registered, , ) = proofOfHumanity.getSubmissionInfo(
+          _submissionID
+      );
+      require(
+          registered == _registered,
+          registered
+              ? "The submission is still registered in Proof Of Humanity."
+              : "The submission is not registered in Proof Of Humanity."
+      );
+      _;
+  }
+
+  modifier isAccruing(address _submissionID, bool _accruing) {
+      bool accruing = lastMintedSecond[human] != 0;
+      require(
+          accruing == _accruing,
+          accruing
+              ? "The submission is already accruing UBI."
+              : "The submission is not accruing UBI."
+      );
+      _;
+  }
+
+  /** @dev Constructor.
+  *  @param initialSupply for the UBI token as integer
+  *  @param _accruedPerSecond How much of the token is accrued per block.
+  *  @param _proofOfHumanity The Proof Of Humanity registry to reference.
+  */
   constructor(
     uint256 initialSupply,
-    uint256 basicIncomeSupplyPerSecond
+    uint256 _accruedPerSecond,
+    IProofOfHumanity _proofOfHumanity,
   ) public ERC20("Democracy Earth", "UBI") {
-      mintingPerSecond = basicIncomeSupplyPerSecond * 10**18;
+      accruedPerSecond = _accruedPerSecond;
+      proofOfHumanity = _proofOfHumanity;
       _mint(msg.sender, initialSupply * 10**18);
   }
 
-  // UBI function
-  function basicIncome(uint256 newSupply, address human) public {
+  /* External */
+
+  /** @dev Universal Basic Income mechanism
+  *  @param human The submission ID.
+  */
+  function getBasicIncome(address human)
+    external isRegistered(human, true) 
+    external isAccruing(human, true) 
+  {
     require(human != address(0), "human cannot be 0");
+    require(human == msg.sender, "human must be sender");
 
-    lastMintedTime[human] = now;
+    uint256 elapsedTime = (now - lastMintedSecond[human]);
+    uint256 newSupply = elapsedTime * accruedPerSecond;
 
-    _mint(human, newSupply * 10**18);
+    lastMintedSecond[human] = now;
+
+    _mint(human, newSupply);
+  }
+
+  /** @dev Starts accruing UBI for a registered submission.
+  *  @param _submissionID The submission ID.
+  */
+  function startAccruing(address human)
+      external
+      isRegistered(human, true)
+      isAccruing(human, false)
+  {
+      lastMintedSecond[human] = now;
   }
 }
