09464d604e17bdaa9fc221843746ae289d8ae7f4
santisiri <santisiri@gmail.com>
2020-12-23 22:57:11 +0100

    tabs

--- contracts/UBI.sol
+++ contracts/UBI.sol
@@ -15,20 +15,20 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
  * @dev See https://github.com/Proof-Of-Humanity/Proof-Of-Humanity.
  */
 interface IProofOfHumanity {
-    enum Status {None, Vouching, PendingRegistration, PendingRemoval}
+  enum Status {None, Vouching, PendingRegistration, PendingRemoval}
 
-    function getSubmissionInfo(address _submissionID)
-        external
-        view
-        returns (
-            Status status,
-            uint64 submissionTime,
-            uint64 renewalTimestamp,
-            uint64 index,
-            bool registered,
-            bool hasVouched,
-            uint256 numberOfRequests
-        );
+  function getSubmissionInfo(address _submissionID)
+    external
+    view
+    returns (
+      Status status,
+      uint64 submissionTime,
+      uint64 renewalTimestamp,
+      uint64 index,
+      bool registered,
+      bool hasVouched,
+      uint256 numberOfRequests
+    );
 }
 
 
@@ -53,8 +53,8 @@ contract UBI is ERC20Burnable  {
 
   /// @dev Verifies sender has ability to modify governed parameters.
   modifier onlyByGovernor() {
-      require(governor == msg.sender, "The caller is not the governor.");
-      _;
+    require(governor == msg.sender, "The caller is not the governor.");
+    _;
   }
 
   /** @dev is Registered as Proof of Human.
@@ -62,16 +62,16 @@ contract UBI is ERC20Burnable  {
   *  @param _registered if it's registered as valid human.
   */
   modifier isRegistered(address _submissionID, bool _registered) {
-      (, , , , bool registered, , ) = proofOfHumanity.getSubmissionInfo(
-          _submissionID
-      );
-      require(
-          registered == _registered,
-          registered
-              ? "The submission is still registered in Proof Of Humanity."
-              : "The submission is not registered in Proof Of Humanity."
-      );
-      _;
+    (, , , , bool registered, , ) = proofOfHumanity.getSubmissionInfo(
+      _submissionID
+    );
+    require(
+      registered == _registered,
+      registered
+        ? "The submission is still registered in Proof Of Humanity."
+        : "The submission is not registered in Proof Of Humanity."
+    );
+    _;
   }
 
   /** @dev is already accruing token subsidy
@@ -79,14 +79,14 @@ contract UBI is ERC20Burnable  {
   *  @param _accruing if its actively accruing value.
   */
   modifier isAccruing(address human, bool _accruing) {
-      bool accruing = lastMintedSecond[human] != 0;
-      require(
-          accruing == _accruing,
-          accruing
-              ? "The submission is already accruing UBI."
-              : "The submission is not accruing UBI."
-      );
-      _;
+    bool accruing = lastMintedSecond[human] != 0;
+    require(
+      accruing == _accruing,
+      accruing
+        ? "The submission is already accruing UBI."
+        : "The submission is not accruing UBI."
+    );
+    _;
   }
 
   /** @dev Constructor.
@@ -99,9 +99,9 @@ contract UBI is ERC20Burnable  {
     uint256 _accruedPerSecond,
     IProofOfHumanity _proofOfHumanity
   ) public ERC20("Democracy Earth", "UBI") {
-      accruedPerSecond = _accruedPerSecond;
-      proofOfHumanity = _proofOfHumanity;
-      _mint(msg.sender, initialSupply * 10**18);
+    accruedPerSecond = _accruedPerSecond;
+    proofOfHumanity = _proofOfHumanity;
+    _mint(msg.sender, initialSupply * 10**18);
   }
 
   /* External */
@@ -129,11 +129,11 @@ contract UBI is ERC20Burnable  {
   *  @param human The submission ID.
   */
   function startAccruing(address human)
-      external
-      isRegistered(human, true)
-      isAccruing(human, false)
+    external
+    isRegistered(human, true)
+    isAccruing(human, false)
   {
-      lastMintedSecond[human] = block.timestamp;
+    lastMintedSecond[human] = block.timestamp;
   }
 
   /** @dev Allows anyone to report a submission that
@@ -143,20 +143,20 @@ contract UBI is ERC20Burnable  {
   *  @param human The submission ID.
   */
   function reportRemoval(address human)
-      external
-      isRegistered(human, false)
-      isAccruing(human, true)
+    external
+    isRegistered(human, false)
+    isAccruing(human, true)
   {
-      lastMintedSecond[human] = 0;
+    lastMintedSecond[human] = 0;
   }  
 
   /** @dev Changes `accruedPerSecond` to `_accruedPerSecond`.
   *  @param _accruedPerSecond How much of the token is accrued per block.
   */
   function changeAccruedPerSecond(uint256 _accruedPerSecond)
-      external
-      onlyByGovernor
+    external
+    onlyByGovernor
   {
-      accruedPerSecond = _accruedPerSecond;
+    accruedPerSecond = _accruedPerSecond;
   }
 }
