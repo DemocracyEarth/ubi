fbae070608f51fee35354582b70886aeca3b0f11
santisiri <santisiri@gmail.com>
2021-01-02 21:21:59 +0100

    first kovan deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,9 +7,15 @@ require("solidity-coverage");
 
 require("./scripts/tasks");
 
-const INFURA_API_KEY = "";
-const MAINNET_PRIVATE_KEY = "";
-const ROPSTEN_PRIVATE_KEY = "";
+// Go to https://infura.io/ and create a new project
+// Replace this with your Infura project ID
+const INFURA_API_KEY = "269a4106080549898dcc50dbb84754f3";
+
+// Replace this private key with your Kovan account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const KOVAN_SECRET = "4611cc522ed9c253198bf66adb599a843c8731e617ed3261ccb755225f16335b";
 const ETHERSCAN_API_KEY = "";
 
 module.exports = {
@@ -17,9 +23,9 @@ module.exports = {
     develop: {
       url: "http://localhost:8545",
     },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${KOVAN_SECRET}`]
     },
     coverage: {
       url: "http://localhost:8555"
