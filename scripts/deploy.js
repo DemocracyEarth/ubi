9c9b604778d9b0a318ecfb12fcefe3f71f7f9ef8
santisiri <santisiri@gmail.com>
2021-01-02 22:44:26 +0100

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+const deploymentParams = require('../deployment-params');
+
 async function main() {
 
   const [deployer] = await ethers.getSigners();
@@ -10,7 +12,12 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const Token = await ethers.getContractFactory("UBI");
-  const token = await Token.deploy();
+  const token = await Token.deploy(
+    deploymentParams.INITIAL_SUPPLY, 
+    deploymentParams.TOKEN_NAME, 
+    deploymentParams.TOKEN_SYMBOL, 
+    deploymentParams.ACCRUED_PER_SECOND, 
+    deploymentParams.PROOF_OF_HUMANITY_KOVAN);
 
   console.log("Token address:", token.address);
 }
