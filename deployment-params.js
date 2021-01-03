705f3fabaa9933ea8cf0856633540fe13e7e25b3
santisiri <santisiri@gmail.com>
2021-01-03 14:32:50 +0100

    earth token

--- deployment-params.js
+++ deployment-params.js
@@ -1,6 +1,6 @@
 // Deployment params
-module.exports.INITIAL_SUPPLY = '10000000000000000000000000'
-module.exports.TOKEN_NAME = "Universal Basic Income"
-module.exports.TOKEN_SYMBOL = "UBI"
-module.exports.ACCRUED_PER_SECOND = '100000000'
-module.exports.PROOF_OF_HUMANITY_KOVAN = '0x413752ADd5ff51CC1928FA73BEFA65b37dEB8730'
+module.exports.INITIAL_SUPPLY = '10000000000000000000000000';
+module.exports.TOKEN_NAME = "Universal Basic Income";
+module.exports.TOKEN_SYMBOL = "EARTH";
+module.exports.ACCRUED_PER_SECOND = '100000000000000';
+module.exports.PROOF_OF_HUMANITY_KOVAN = '0x413752ADd5ff51CC1928FA73BEFA65b37dEB8730';
