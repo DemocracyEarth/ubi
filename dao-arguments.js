3918551041b4a9ca6eac2efe5889a253331c1a32
Santiago Siri <santisiri@gmail.com>
2021-01-17 01:09:05 +0100

    dao verification arguments for etherscan

---
+++ dao-arguments.js
@@ -0,0 +1,11 @@
+// Used for verification of contract on Etherscan
+module.exports = [
+  '0xEdB47F691a77022f03817f80FEc09aCc363747aE',
+  ['0xDdAdE19B13833d1bF52c1fe1352d41A8DD9fE8C9'],
+  17280,
+  35,
+  35,
+  '1000000000000000000',
+  3,
+  '10000000000'
+];
