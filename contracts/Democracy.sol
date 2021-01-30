0df0168970bba3a0ae5c44b951577c2eb5d5b975
Santiago Siri <santisiri@gmail.com>
2021-01-30 12:41:05 +0100

    leave lessons in the packages

--- contracts/Democracy.sol
+++ contracts/Democracy.sol
@@ -67,7 +67,7 @@ contract Democracy is IERC20 {
     }
 
     // ******************** //
-    // *      Snaptshot   * //
+    // *    Snapshot      * //
     // ******************** //
 
     /** @dev External function for Snapshot event emitter only accessible by deployer.  */
@@ -103,21 +103,7 @@ contract Democracy is IERC20 {
         require(snapshotId > 0, "ERC20Snapshot: id is 0");
         // solhint-disable-next-line max-line-length
         require(snapshotId <= _currentSnapshotId.current(), "ERC20Snapshot: nonexistent id");
-
-        // When a valid snapshot is queried, there are three possibilities:
-        //  a) The queried value was not modified after the snapshot was taken. Therefore, a snapshot entry was never
-        //  created for this id, and all stored snapshot ids are smaller than the requested one. The value that corresponds
-        //  to this id is the current one.
-        //  b) The queried value was modified after the snapshot was taken. Therefore, there will be an entry with the
-        //  requested id, and its value is the one to return.
-        //  c) More snapshots were created after the requested one, and the queried value was later modified. There will be
-        //  no entry for the requested id: the value that corresponds to it is that of the smallest snapshot id that is
-        //  larger than the requested one.
-        //
-        // In summary, we need to find an element in an array, returning the index of the smallest value that is larger if
-        // it is not found, unless said value doesn't exist (e.g. when all values are smaller). Arrays.findUpperBound does
-        // exactly this.
-
+        
         uint256 index = snapshots.ids.findUpperBound(snapshotId);
 
         if (index == snapshots.ids.length) {
