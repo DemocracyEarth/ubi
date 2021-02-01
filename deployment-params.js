6a70e0b9d14b03b6719098cd16569e06507e1c5b
Santiago Siri <santisiri@gmail.com>
2021-02-01 18:22:50 +0100

    v0.1.7

--- deployment-params.js
+++ deployment-params.js
@@ -7,14 +7,4 @@ module.exports.PROOF_OF_HUMANITY_KOVAN = '0x413752ADd5ff51CC1928FA73BEFA65b37dEB
 module.exports.PROXY_CONTRACT_ADDRESS = '0xDdAdE19B13833d1bF52c1fe1352d41A8DD9fE8C9';
 module.exports.PROXY_ADMIN = '0x33B67C1B1161421114F5476597Bc054232B47C06';
 
-// Deployment params for DAO
-module.exports.SUMMONER = '0xEdB47F691a77022f03817f80FEc09aCc363747aE'
-module.exports.TOKEN = '0xDdAdE19B13833d1bF52c1fe1352d41A8DD9fE8C9'
-module.exports.PERIOD_DURATION_IN_SECONDS = 17280
-module.exports.VOTING_DURATON_IN_PERIODS = 35
-module.exports.GRACE_DURATON_IN_PERIODS = 35
-module.exports.PROPOSAL_DEPOSIT = '1000000000000000000' // Large numbers should be string or big numbers
-module.exports.DILUTION_BOUND = 3
-module.exports.PROCESSING_REWARD = '10000000000'
-
 // @NOTE: decimal counter string: 01234567890abcdefg
