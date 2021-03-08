69b107a5ca6eebfc44493c3a56518f51feebd130
Santiago Siri <santisiri@gmail.com>
2021-03-08 13:42:00 +0100

    deployment params

--- deployment-params.js
+++ deployment-params.js
@@ -1,8 +1,8 @@
 // Deployment params for UBI
-module.exports.INITIAL_SUPPLY = '1000000000000000000000000000';
+module.exports.INITIAL_SUPPLY = '10000000000000000000000000';
 module.exports.TOKEN_NAME = "Universal Basic Income";
 module.exports.TOKEN_SYMBOL = "UBI";
-module.exports.ACCRUED_PER_SECOND = '10000000000000000';
+module.exports.ACCRUED_PER_SECOND = "14000000000000"
 
 module.exports.PROOF_OF_HUMANITY_MAINNET = '';
 module.exports.PROXY_CONTRACT_ADDRESS_MAINNET = '';
@@ -14,4 +14,4 @@ module.exports.PROXY_ADMIN_KOVAN = '0x33B67C1B1161421114F5476597Bc054232B47C06';
 
 module.exports.VOTE_NAME = "Democracy Earth";
 module.exports.VOTE_SYMBOL = "VOTE";
-// @NOTE: decimal counter string: 01234567890abcdefg
+// @NOTE: decimal counter string: 012345678901234567
