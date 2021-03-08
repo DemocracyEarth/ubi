6393797cdedf7c83ed88e9af940b2e6551e43a25
Santiago Siri <santisiri@gmail.com>
2021-03-08 13:40:16 +0100

    mainnet parameters

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -3,7 +3,7 @@ const deploymentParams = require('../deployment-params');
 async function main() {
   const UBIv2 = await ethers.getContractFactory("UBI");
   console.log("Preparing upgrade...");
-  const UBIv2Address = await upgrades.prepareUpgrade(deploymentParams.PROXY_CONTRACT_ADDRESS, UBIv2, { unsafeAllowCustomTypes: true });
+  const UBIv2Address = await upgrades.prepareUpgrade(deploymentParams.PROXY_CONTRACT_ADDRESS_KOVAN, UBIv2, { unsafeAllowCustomTypes: true });
   console.log("A new version of UBI was deployed at:", UBIv2Address);
 }
 
